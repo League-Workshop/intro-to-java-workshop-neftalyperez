1aec716a531a69ddcf1b7af1c505c815f246f77f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:55:09 -0700

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+		Robot R2_D2=new Robot();
+		R2_D2.changeRobot("https://sm.ign.com/ign_ap/news/s/stan-lee-d/stan-lee-drops-big-hero-6-sequel-news_hkmy.jpg");
+		R2_D2.penDown();
+		int sides=1000;
+	    int angle=360/sides;
+		for(int i=0;i<sides;i++) {
+			R2_D2.move(100);
+		R2_D2.turn(10);
+		R2_D2.setSpeed(10);
+		}
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,23 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot R2_D2=new Robot();
 		// 5. Set your robot's pen down 
-		
+		R2_D2.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		R2_D2.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
-			// 7. Change the pen color to random
-		
+	    for(int i=0;i<200;i++){
+	    // 7. Change the pen color to random
+		R2_D2.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			R2_D2.move(5*i);	
+	    
 			// 2. Turn the robot 360/7 degrees to the right
+			R2_D2.turn(360/3);
 		
 			// 8. Set the pen width to i
 			
+	    }	
 	}
 }
